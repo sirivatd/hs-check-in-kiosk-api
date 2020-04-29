a5942bd7c796d02896d54cd89de6689659567893
Don Sirivat <don@joinhandshake.com>
2020-04-28 21:26:30 -0500

    Fix

--- frontend/api.jsx
+++ frontend/api.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Root from './components/root';
-import configureStore from './store/store';
-
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root)
+  ReactDOM.render(<h2>hkjhkj</h2>, root)
 })
